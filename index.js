855248aa64b15adcc4e2a509981414e78a27cd33
zypeh <zypeh.geek@gmail.com>
2016-03-19 01:28:06 +0800

    initialize

---
+++ index.js
@@ -0,0 +1,36 @@
+"use strict";
+// Dependencies
+const passport = require('koa-passport')
+    , mongoose = require('mongoose')
+    , colors = require('colors')
+    , http = require('http')
+    , koa = require('koa')
+    , fs = require('fs')
+    ;
+
+// Koa app
+const app = koa();
+
+// Configs
+const config = require('config');
+
+// Database
+mongoose.connect(config.mongo.url);
+
+mongoose.connection.on('error', function() {
+  console.error.bind('[ ! ] Connection error: '.red);
+});
+
+mongoose.connection.once('open', function() {
+  console.log('[ * ] Database connection open.'.gren);
+});
+
+// Module
+require('./lib/passport')(passport, config);
+require('./lib/koa')(app, config, passport);
+
+// Routes
+require('./routes/routes')(app, passport);
+
+// HTTP Server Instance
+http.create(app.callback()).listen(3000);
---
+++ routes/routes.js
@@ -0,0 +1,25 @@
+"use stric";
+
+// Dependencies
+var Router = require('koa-router')
+  ;
+
+// Secured path middleware
+var secured = function *(next) {
+  if (!this.isAuthenticated()) this.status = 401;
+  yield next;
+};
+
+// Routes
+module.exports = function(app, passport) {
+  const route = new Router();
+
+  route
+    .get('/auth/facebook', passport.authenticate('facebook'))
+    .get('/auth/facebook/callback', passport.authenticate('facebook', {
+      successRedirect: '/magic',
+      faileureRedirect: '/'
+    }))
+    ;
+    // Views... TODO
+}
