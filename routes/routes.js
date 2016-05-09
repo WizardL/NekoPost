07d5727cc9faabed4d528c98c1697dc5044a4536
Jonathan Goh <jonathan.goh333@gmail.com>
2016-05-09 17:12:13 +0800

    Fix 404.

--- routes/routes.js
+++ routes/routes.js
@@ -25,7 +25,6 @@ module.exports = function (app, passport) {
 
     .get('/', pages.index)
     .get('/abuse/:postid', pages.abuse)
-    .get('/404', pages.404)
     .post('/upload', handler.upload)
   ;
   
