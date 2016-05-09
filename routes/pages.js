d6d1a186caf26307973151500bc858f135c70688
sooxiaotong <sooxiaotong@gmail.com>
2016-05-09 17:01:23 +0800

    404

--- routes/pages.js
+++ routes/pages.js
@@ -7,6 +7,13 @@ exports.index = function* (next) {
   return yield next;
 };
 
+// 404 page
+exports.404 = function* (next) {
+  console.log(JSON.stringify(this));
+  yield this.render('404.html');
+  return yield next;
+};
+
 exports.abuse = function* (next) {
   var id = this.params.postid;
   this.render('abuse_page', { post_id: id });
