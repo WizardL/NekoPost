d8e348c28c8f6ad9e75fc50c7ca9ff6ffb2e3f34
zypeh <zypeh.geek@gmail.com>
2016-07-10 01:05:58 +0800

    promisified the mongoose database connector

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 "use strict";
 
 import app from './src'
-
 import colors from 'colors'
 
+import { development, test, production } from './config'
 const port = process.env.PORT || 3000
+const DBConfig = (process.env.NODE_DEV == 'production') ? production : development
 
-(async () => {
-  await app.listen(port)
+;(async () => {
+  try {
+    const info = await DatabaseConnector(DBConfig)
+    console.log(`[${'*'.green}]  Connected to ${info.host}:${info.port}/${info.name}`)
+  } catch (err) {
+    console.log(`[${'!'.red}]  Unable to connect to database`)
+  }
+  await app.listen(port) 
   console.log(`[${'@'.green}]  Server listening on port ${port}`)
+  
+  // Gracefully close when INTERRUPT Signal occurred.
+  process.on('SIGINT', () => mongoose.connection.close(() => process.exit(0)))
+  
 })()
--- src/db.js
+++ src/db.js
@@ -3,17 +3,14 @@
 import colors from 'colors'
 import mongoose from 'mongoose'
 
-import { development, test, production } from './config'
-const DBConfig = (process.env.NODE_ENV == 'production') ? production : development
+export function DatabaseConnector(uri) {
+  return new Promise((resolve, reject) => {
+    mongoose.connection
+      .on('error', error => reject(error))
+      .on('close', () => console.log(`[${'!'.red}]Database connection closed`))
+      .once('open', () => resolve(mongoose.connections[0]))
 
-export default mongoose
+    mongoose.connect(uri) 
+  })
+}
 
-mongoose.connect(DBConfig)
-
-mongoose.connection
-  .on('error', error => console.log(`[${'!'.red}]  Unable to connect to database'}`))
-  .on('close', () => console.log(`[${'!'.red}]  Database connection closed.`))
-  .once('open', () => console.log(`[${'*'.green}  Connected to ${mongoose.conneections[0].host}:${mongoose.connections[0].port}/${mongoose.connections[0].name}]`))
-
-// Gracefully close when INTERRUPT Signal occurred.
-process.on('SIGINT', () => mongoose.connection.close(() => process.exit(0)))
