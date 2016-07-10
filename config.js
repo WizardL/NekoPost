ed4c96a4bb67f573dad0e7bd09644a8328e2b5d1
Jonathan Goh <jonathan.goh333@gmail.com>
2016-07-10 16:45:22 +0800

    Typo..

--- config.js
+++ config.js
@@ -6,6 +6,6 @@ export const production  = 'mongodb://localhost/misato-prod'
 export const test        = 'mongodb://localhost/misato-test'
 
 // Recaptcha related
-export const recaptcha_development = { key: '', secret: ''}
-export const recaptcha_prodution   = { key: '', secret: ''}
-export const recaptcha_test        = { key: '6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI', secret: '6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe'} //test key from https://developers.google.com/recaptcha/docs/faq
+export const recaptcha_development = { key: '', secret: '' }
+export const recaptcha_prodution   = { key: '', secret: '' }
+export const recaptcha_test        = { key: '6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI', secret: '6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe' } //test key from https://developers.google.com/recaptcha/docs/faq
