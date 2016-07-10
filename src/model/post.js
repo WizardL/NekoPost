e14b4525bef2968da819e5ec4f658d4ea8aae6f1
zypeh <zypeh.geek@gmail.com>
2016-07-10 19:34:14 +0800

    Moved models inside /src, changed schema

---
+++ src/model/post.js
@@ -0,0 +1,35 @@
+"use strict"
+
+import mongoose from 'mongoose'
+import shortid from 'shortid'
+
+// Will check if the unique id `_id` is used in database or not
+const idGenerator = (ctx, callback) => {
+  let newId = shortid.generate()
+  ctx.constructor.findOne({ _id: newId }).then((course) => {
+    if (course)
+      idGenerator(ctx, callback)
+    else {
+      ctx._id = newId
+      next()
+    }
+  }, (err) => {
+    next(err)
+  })
+}
+
+const PostSchema = new mongoose.Schema({
+  _id: { type: String, unique: true },
+  status: { delivered: { type: Boolean } },
+  reporter: [{ type: String }],
+  created_on: { type: Date, default: Date.now }
+})
+
+// save hook
+PostSchema.pre('save', function(next) {
+  const post = this
+  idGenerator(post, next)
+})
+
+// Model initialization
+export default mongoose.model('Post', PostSchema)
