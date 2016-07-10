c73baaf0bd4a47d7691e1cdb36ff3150a54a6e24
Jonathan Goh <jonathan.goh333@gmail.com>
2016-07-10 15:50:24 +0800

    Add config for recaptcha.

--- config.js
+++ config.js
@@ -4,3 +4,8 @@
 export const development = 'mongodb://localhost/misato-dev'
 export const production  = 'mongodb://localhost/misato-prod'
 export const test        = 'mongodb://localhost/misato-test'
+
+// Recaptcha related
+export const recaptcha_development = { key: '', secret: ''}
+export const recaptcha_prodution   = { key: '', secret: ''}
+export const recaptcha_test        = { key: '6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI', secret: '6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe'} //test key from https://developers.google.com/recaptcha/docs/faq
