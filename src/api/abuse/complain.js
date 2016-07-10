0db2a907a5d8d7a51fb143d84888ea56b6fe58be
Jonathan Goh <jonathan.goh333@gmail.com>
2016-07-10 19:14:40 +0800

    Add recaptcha verify.

--- src/api/abuse/complain.js
+++ src/api/abuse/complain.js
@@ -10,5 +10,20 @@ export default (router) => {
 }
 
 async function complain_handler (ctx, next) {
+  //Verify Recatpcha
+  try {
+    if (!ctx.body["g-recaptcha-response"]) {
+      ctx.throw(500, 'Please remember to complete the human verification.')
+      return;
+    }
+    await recaptcha.promise(RecaptchaConfig.secret, this.body["g-recaptcha-response"], ctx.request.ip);
+    ctx.body = { msg: 'Good!' };
+  } catch (err) {
+    if (typeof err === 'string')
+      ctx.throw(500, err);
+    else
+      ctx.throw(500, err);
+  }
+  
   // TODO
 }
