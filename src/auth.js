e20b826e608b8937b7ab75af2604aed886f16e0d
zypeh <zypeh.geek@gmail.com>
2016-07-23 13:15:35 +0800

    facebook session save to mongodb

--- src/auth.js
+++ src/auth.js
@@ -7,6 +7,7 @@ import compose from 'koa-compose'
 import convert from 'koa-convert'
 import passport from 'koa-passport'
 import session from 'koa-generic-session'
+import  mongoStore from 'koa-generic-session-mongo'
 import { Strategy as FacebookStrategy } from 'passport-facebook'
 
 passport.use(new FacebookStrategy({
@@ -23,7 +24,9 @@ export default function auth() {
   return compose([
 
     // session 
-    convert(session()),
+    convert(session({
+      store: new mongoStore()
+    })),
 
     // passport initialization
     passport.initialize(),
