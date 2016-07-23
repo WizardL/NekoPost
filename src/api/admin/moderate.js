73722d80c25c228a00e25142b0e5014eaafcb6df
zypeh <zypeh.geek@gmail.com>
2016-07-23 17:40:54 +0800

    cleanup unused code

--- src/api/admin/moderate.js
+++ src/api/admin/moderate.js
@@ -2,9 +2,6 @@
 
 import { isAuthenticated } from '../../auth'
 
-import compose from 'koa-compose'
-import convert from 'koa-convert'
-
 export default (router) => {
 
   router
@@ -23,7 +20,7 @@ export default (router) => {
 }
 
 async function post_dashboard(ctx, next) {
-  // TODO
+  
 }
 
 async function post_accepted(ctx, next) {
