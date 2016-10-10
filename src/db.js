507fa6df03c8d5335b90baa2f14cb6cc597f9402
Jonathan Goh <jonathan.goh333@gmail.com>
2016-10-10 12:03:57 +0800

    Fix `UnhandledPromiseRejectionWarning` on `db.js` .

--- src/db.js
+++ src/db.js
@@ -4,14 +4,18 @@ import colors from 'colors'
 import mongoose from 'mongoose'
 
 export function DatabaseConnector(uri) {
-  return new Promise((resolve, reject) => {
+  return new Promise(async (resolve, reject) => {
 
     mongoose.connection
       .on('error', error => reject(error))
       .on('close', () => console.log(`[${'!'.red}]Database connection closed`))
       .once('open', () => resolve(mongoose.connections[0]))
-
-    mongoose.connect(uri)
+    
+    try {
+      await mongoose.connect(uri)
+    } catch (error) {
+      reject(error)
+    }
 
     // Gracefully shutdown when INTERRUPT signal occurred
     process.on('SIGINT', () => mongoose.connection.close(() => process.exit(0)))
