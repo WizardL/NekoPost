4259aac306ebb71f611fd172907a0d11e0ef6fda
zypeh <zypeh.geek@gmail.com>
2016-10-11 01:02:30 +0800

    adopt redux trend; use vuex

--- public/misato-frontend/src/main.js
+++ public/misato-frontend/src/main.js
@@ -1,20 +1,31 @@
+'use strict'
+
 import Vue from 'vue'
-import VueRouter from 'vue-router'
+import App from './App.vue'
+import store from './store'
+import router from './router'
+import * as filters from './filters'
+import { sync } from 'vuex-router-sync'
 
-import { configRouter } from './router-config'
+// sync the router with the vuex store.
+// this registers `store.state.route`
+sync(store, router)
 
-Vue.use(VueRouter)
-
-const App = Vue.extend(require('./App'))
-
-const router = new VueRouter({
-  mode: 'history',
-  routes: App
+// register global utility filters.
+Object.keys(filters).forEach(key => {
+  Vue.filter(key, filters[key])
 })
 
+// create the app instance.
+// here we inject the router and store to all child components,
+// making them available everywhere as `this.$router` and `this.$store`.
 const app = new Vue({
-  router
-}).$mount('#app')
+  router,
+  store,
+  ...App // Object spread copying everything from App.vue
+})
 
-// debugging
-window.router = router
+// expose the app, the router and the store.
+// note we are not mounting the app here, since bootstrapping will be
+// different depending on whether we are in a browser or on the server.
+export { app, router, store }
