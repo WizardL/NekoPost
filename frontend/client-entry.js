a4771de83142edc05a5d26b94a267fbf5dbadfe8
zypeh <zypeh.geek@gmail.com>
2017-02-24 12:20:42 +0800

    Remove unused {client, server}-entry

--- frontend/client-entry.js
+++
@@ -1,9 +0,0 @@
-require('es6-promise').polyfill() // Polyfill for ES6 start here
-import { app, store } from './main'
-
-// prime the store with server-initialized state.
-// the state is determined during SSR and inlined in the page markup
-store.replaceState(window.__INITIAL_STATE__)
-
-// actually mount to DOM
-app.$mount('#app')
