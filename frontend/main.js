f96743489d70812b32ae92cc3748a06db72e00e9
zypeh <zypeh.geek@gmail.com>
2017-02-24 19:16:47 +0800

    spread syntax enabled; babelrc configuration

--- frontend/main.js
+++ frontend/main.js
@@ -4,7 +4,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
 import router from './router'
-import * as filters from './filters'
+import * as filters from './utils/filters'
 import { sync } from 'vuex-router-sync'
 
 // sync the router with the vuex store.
@@ -26,9 +26,11 @@ const app = new Vue({
   router,
   store,
   ...App // Object spread copying everything from App.vue
-}).$mount('#app')
+})
 
-// expose the app, the router and the store.
-// note we are not mounting the app here, since bootstrapping will be
-// different depending on whether we are in a browser or on the server.
-export { app, router, store }
+// mount the vue instance to DOM
+store.replaceState(window.__INITIAL_STATE__)
+app.$mount('#app')
+
+if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator)
+  navigator.serviceWorker.register('/service-worker.js')
