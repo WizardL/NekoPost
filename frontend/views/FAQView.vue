18e462b82443965447ec006531b8159dddfa4dab
zypeh <zypeh.geek@gmail.com>
2017-02-26 18:22:37 +0800

    remove unused vue files

--- frontend/views/FAQView.vue
+++ frontend/views/FAQView.vue
@@ -27,5 +27,4 @@
 </style>
 
 <script>
-loadImg: (path) => require(path)
 </script>
--- frontend/views/Post.vue
+++ frontend/views/Post.vue
@@ -1,15 +1,3 @@
 <template>
-  <div class="post">
-    <div class="s-wrapper">
-      <div class="media"><img src="../assets/logo.png"></div>
-      <div class="col"><p>大图测试</p></div>
-      <div class="col">
-        <div class="meta">
-          <div class="desc"><a href="#">18/7/2016</a></div>
-          <div class="desc"><a href="#">#色气</a></div>
-          <div class="desc"><a href="#">#动漫</a></div>
-        </div>
-      </div>
-    </div>
-  </div>
+
 </template>
