64c6eb880896cfe46695391affb996e0998ba34d
zypeh <zypeh.geek@gmail.com>
2017-06-05 23:53:25 +0800

    canvas programming

--- frontend/views/PostView.vue
+++ frontend/views/PostView.vue
@@ -18,10 +18,55 @@
 </template>
 
 <script>
+import dl from 'downloadjs'
+const $ = document.querySelector.bind(document)
+
 export default {
     name: 'post-view',
-
+    data() {
+        return {
+            form: {
+                content: '😽 Post something awesome here',
+            },
+            canvasSize: {
+                width: 1280 * 2,
+                height: 720 * 2,
+            }
+        }
+    },
+    watch: {
+        form: {
+            handler() {
+                this.draw()
+            },
+            deep: true,
+        }
+    },
+    mounted() { this.draw() },
+    computed: {
+        fontSize() {
+            return {
+                
+            }
+        }
+    },
     methods: {
+        draw() {
+            const existing = $('#canvas')
+            if (existing)
+                this.$ref.output.removeChild(existing)
+            
+            const canvas = document.createElement('canvas')
+            canvas.id = 'canvas'
+            canvas.width = this.canvasSize.width
+            canvas.height = this.canvasSize.height
+            
+            const context = canvas.getContext('2d')
+            // For retina display
+            context.scale(2,2)
+
+
+        }
     }
 }
 
